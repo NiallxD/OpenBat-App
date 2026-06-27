1b8b67c8e912864d1dfe140ade960a987ea90a42
Niall Bell <niallbell@hotmail.co.uk>
2026-06-27 11:39:41 -0700

    Initial Commit

---
+++ OpenBat/OpenBatApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenBatApp.swift
+//  OpenBat
+//
+//  Created by Niall Bell on 27/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct OpenBatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
